57215ee0505a1f9b2beae709d564a9a1849e6e13
Costantinosfisseha <152017563+Costantinosfisseha@users.noreply.github.com>
2025-09-19 15:56:45 -0700

    index.cpp

---
+++ Week_5/index.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include "index.h"
+using namespace std;
+
+void showMenu() {
+    cout << "1. Add\n2. View\n3. Exit\n";
+}
+
+void Add() {
+    cout << "You are in the Add function\n";
+}
+
+void View() {
+    cout << "You are in the View function\n";
+}
+
+void Exit() {
+    cout << "You are in the Exit function\n";
+
+}
