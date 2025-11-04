d98c01611431a7fae9a76510f33a1f26d0acce17
Costantinosfisseha <152017563+Costantinosfisseha@users.noreply.github.com>
2025-11-04 13:01:44 -0800

    Delete week_10/#include <string> #include <iostream> using namespace std; class Item{     public:/index.h

--- week_10/#include <string> #include <iostream> using namespace std; class Item{     public:/index.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <iostream>
-using namespace std;
-class Item{
-    public://so it can be accessed from outside too 
-    string name;
-    int quantity;
-};
-void searchItem(Item items[], int size, string search_name);
